717d5e3e9194deefbe47400ce1dd70429dc9b756
fikrua <fikruayal@gmail.com>
2019-05-16 12:51:51 -0400

    springboot303

---
+++ src/main/java/boot303/boot303/Boot303Application.java
@@ -0,0 +1,13 @@
+package boot303.boot303;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot303Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot303Application.class, args);
+	}
+
+}
---
+++ src/main/java/boot303/boot303/CourseRepository.java
@@ -0,0 +1,6 @@
+package boot303.boot303;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course,Long> {
+}
